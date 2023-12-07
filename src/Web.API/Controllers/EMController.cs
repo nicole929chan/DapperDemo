7071bf1f169a1dd3944039eeb81ffa2bcaf991cb
Nicole <nuling.huang@ehsn.com.tw>
2023-12-07 16:36:37 +0800

    Extension Methods attach to Instance

--- src/Web.API/Controllers/EMController.cs
+++ src/Web.API/Controllers/EMController.cs
@@ -8,11 +8,16 @@ public class EMController : ControllerBase
     [HttpGet]
     public IActionResult Get()
     {
-        string demo = "Hello";
+        // string demo = "Hello";
 
         // .CombineWords()不須帶任何參數
         // demo本身就會成為擴展方法的第一個參數(message)
-        return Ok(demo.CombineWords());
+        // return Ok(demo.CombineWords());
+
+        SimpleLogger logger = new();  // 因為下方的程式碼, .NET編譯時已經知道logger已經具備LogError()
+        logger.LogError("This is an error");
+
+        return Ok();
     }
 }
 
@@ -24,5 +29,31 @@ public static class Extensions
     public static string CombineWords(this string message)
     {
         return message + " World";
+    }
+}
+
+public static class ExtensionSimpleLogger
+{
+    // LogError是一個擴展方法
+    // 它被掛到SimpleLogger實例上
+    // 擴展方法被觸發時所帶進來的參數由message接收
+    public static void LogError(this SimpleLogger logger, string message)
+    {
+        // 因為有logger, 所以可以觸發它真正的方法, 而非野橄欖的~
+        // 這裡一律帶的type就是Error (由自己來標準化)
+        logger.Log(message, "Error");
+    }
+}
+// 第三方套件
+public class SimpleLogger
+{
+    public void Log(string message)
+    {
+        // do something
+    }
+
+    public void Log(string message, string messageType)
+    {
+        // do something
     }
 }
